b9184f0333f425a113bdd4b4e7b6e72d4d5a0e04
Fahmi <fahmikurnia04@gmail.com>
2024-02-13 13:03:29 +0700

    add files

---
+++ audio.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("btn");
+const audio = document.getElementById("myAudio");
+
+button.addEventListener("click", function(event) {
+        // Play the audio
+        audio.play();
+      });
