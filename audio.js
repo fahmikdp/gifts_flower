0826b181459c6cb0a4f15a17b18f87f01a1a2a92
Fahmi <fahmikurnia04@gmail.com>
2024-02-13 13:21:14 +0700

    revisi sdikit

--- audio.js
+++ audio.js
@@ -1,10 +1,5 @@
 const button = document.getElementById("btn");
-const audio1 = document.getElementById("myAudio1");
 const audio2 = document.getElementById("myAudio2");
-
-onload = () =>{
-  audio1.play();
-};
 
 button.addEventListener("click", function(event) {
         // Play the audio
